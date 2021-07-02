496e3d91b31d5703c089641d896c92c51cc5c844
elu697 <elyou.dev@gmail.com>
2021-07-02 16:44:56 +0900

    Update setup

---
+++ setup/docker/setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+docker-compose build
+docker-compose up -d
+
+# host bufer tuning
+
+if [ $(uname) = Darwin ]; then
+sudo sysctl net.local.stream.sendspace=2000000
+sudo sysctl net.local.stream.recvspace=2000000
+else
+sudo sysctl --write net.core.rmem_default=10000000
+sudo sysctl --write net.core.wmem_default=10000000
+sudo sysctl --write net.core.rmem_max=10000000
+sudo sysctl --write net.core.wmem_max=10000000
+fi
+
