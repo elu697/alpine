3b27e5bed374edc697393c2d7b6719cab729c889
elu697 <elyou.dev@gmail.com>
2021-09-29 08:07:11 +0000

    Update

--- setup/docker/setup.sh
+++ setup/docker/setup.sh
@@ -2,22 +2,30 @@
 
 set -x
 
-# sudo apt-get -y update
-# sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common aptitude
-# curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-# sudo apt-key fingerprint 0EBFCD88
-# sudo add-apt-repository    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-#     $(lsb_release -cs) \
-#     stable"
-# sudo apt-get -y update
-# sudo apt-get install -y docker-ce
-# sudo docker version
-# sudo usermod -aG docker $USER
+# Add the package repositories
+distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
+curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add -
+curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
 
-# sudo add-apt-repository ppa:graphics-drivers/ppa
-# sudo aptitude -y update
-# sudo aptitude -y install ubuntu-drivers-common
-# sudo aptitude -y install nvidia-driver-470-server
+sudo apt-get update && sudo apt-get install -y nvidia-container-toolkit
+sudo systemctl restart docker
+
+sudo apt-get -y update
+sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common aptitude
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+sudo add-apt-repository    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+    $(lsb_release -cs) \
+    stable"
+sudo apt-get -y update
+sudo apt-get install -y docker-ce
+sudo docker version
+sudo usermod -aG docker $USER
+
+sudo add-apt-repository ppa:graphics-drivers/ppa
+sudo aptitude -y update
+sudo aptitude -y install ubuntu-drivers-common
+sudo aptitude -y install nvidia-driver-470-server
 
 
 docker-compose build
