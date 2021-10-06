71ce18c949bfdf8b52dfa254c6e91ce819813a83
elu697 <elyou.dev@gmail.com>
2021-10-06 17:15:48 +0900

    Update

--- setup/docker/setup.sh
+++ setup/docker/setup.sh
@@ -3,28 +3,28 @@
 set -x
 
 # Add the package repositories
-sudo apt-get -y update
-sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common aptitude
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo apt-key fingerprint 0EBFCD88
-sudo add-apt-repository    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-    $(lsb_release -cs) \
-    stable"
-sudo apt-get -y update
-sudo apt-get install -y docker-ce
-sudo docker version
-sudo usermod -aG docker $USER
+# sudo apt-get -y update
+# sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common aptitude
+# curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+# sudo apt-key fingerprint 0EBFCD88
+# sudo add-apt-repository    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+#     $(lsb_release -cs) \
+#     stable"
+# sudo apt-get -y update
+# sudo apt-get install -y docker-ce
+# sudo docker version
+# sudo usermod -aG docker $USER
 
-sudo add-apt-repository ppa:graphics-drivers/ppa
-sudo aptitude -y update
-sudo aptitude -y install ubuntu-drivers-common
-sudo aptitude -y install nvidia-driver-470-server
+# sudo add-apt-repository ppa:graphics-drivers/ppa
+# sudo aptitude -y update
+# sudo aptitude -y install ubuntu-drivers-common
+# sudo aptitude -y install nvidia-driver-470-server
 
-distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
-curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add -
-curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
-sudo apt-get update && sudo apt-get install -y nvidia-docker2
-sudo systemctl restart docker
+# distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
+# curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add -
+# curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
+# sudo apt-get update && sudo apt-get install -y nvidia-docker2
+# sudo systemctl restart docker
 
 docker-compose build
 docker-compose up -d
