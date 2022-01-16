badc7fcde7ffe1175630ef4f5ac6c5ef9873360a
elu697 <elyou.dev@gmail.com>
2022-01-16 06:15:47 +0000

    Update PLT

---
+++ nln/core/src/main/java/DataType.java
@@ -0,0 +1,2 @@
+package ndn;public enum DataType {
+}
--- nln/core/src/main/java/PLT.java
+++ nln/core/src/main/java/PLT.java
@@ -2,10 +2,11 @@ import net.named_data.jndn.Face;
 import net.named_data.jndn.Name;
 
 import java.lang.reflect.Array;
+import java.time.LocalDateTime;
 import java.util.*;
 
 public class PLT {
-    static class PendingInfo extends Hashtable {
+    static final class PendingInfo {
         Name prefix;
         Face request;
 
@@ -13,12 +14,31 @@ public class PLT {
             this.prefix = prefix;
             this.request = request;
         }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            PendingInfo that = (PendingInfo) o;
+            return Objects.equals(prefix, that.prefix) && Objects.equals(request, that.request);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(prefix, request);
+        }
     }
 
-    static class LearningInfo extends Hashtable {
+    static class LearningInfo {
+        LocalDateTime lastMonitoredTime;
         ArrayList<Face> aliveFaces = new ArrayList<>();
 
         public LearningInfo(){
+            monitored();
+        }
+
+        public void monitored() {
+            lastMonitoredTime = LocalDateTime.now();
         }
 
         public void pushFace(Face face) {
@@ -30,17 +50,18 @@ public class PLT {
         }
     }
 
-    public static PLT shard = new PLT();
-    LinkedHashMap<PendingInfo, PLT.LearningInfo> lhm = new LinkedHashMap<>();
+    volatile public static PLT shard = new PLT();
+    private LinkedHashMap<PendingInfo, PLT.LearningInfo> lhm = new LinkedHashMap<>();
 
     private PLT() {
+
     }
 
     public void push(Name prefix, Face requestFrom, Face alive) {
         PendingInfo pi = new PendingInfo(prefix, requestFrom);
         LearningInfo learningInfo = lhm.getOrDefault(pi, new LearningInfo());
         learningInfo.pushFace(alive);
-        lhm.put(pi, learningInfo);
+        lhm.putIfAbsent(pi, learningInfo);
     }
 
     public void pop(Name prefix, Face requestFrom, Face alive) {
@@ -63,5 +84,20 @@ public class PLT {
     }
 
     public static void main(String[] args) {
+        Face face =  new Face("localhost");
+        Face face2 =  new Face("localhost");
+        PLT.shard.push(new Name("/model/A"), face, face2);
+        PLT.shard.push(new Name("/model/A"), new Face("localhost"), new Face("1"));
+        PLT.shard.getPending().forEach(i -> System.out.println(i.prefix + "," + PLT.shard.getLearning(i).aliveFaces));
+
+
+        System.out.println("\n");
+        PLT.shard.push(new Name("/model/B"), new Face("localhost"), new Face());
+        PLT.shard.pop(new Name("/model/A"), face, face2);
+        PLT.shard.getPending().forEach(i -> System.out.println(i.prefix + "," + PLT.shard.getLearning(i).aliveFaces));
+
+        System.out.println("\n");
+        PLT.shard.push(new Name("/model/A"), new Face("localhost"), new Face("1"));
+        PLT.shard.getPending().forEach(i -> System.out.println(i.prefix + "," + PLT.shard.getLearning(i).aliveFaces));
     }
 }
