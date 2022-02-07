7bdd452d0501dc8097dabe45df13576082d4b837
elu697 <elyou.dev@gmail.com>
2022-02-08 03:28:04 +0900

    Update

--- nln/bash-allnode.sh
+++ nln/bash-allnode.sh
@@ -1,44 +1,52 @@
-set -Cux
+# set -Cux
 
 # for i in {2..20}; do
 #     ssh icnl_lrd_vm$i pwd
 # done
-
-# for i in {2..20}; do
-#     echo $((i % 5))
-#     if test $((i % 5)) -eq 0; then
-#         (ssh icnl_lrd_vm$i "cd /home/docker/alpine/nln && git pull")
-#     else
-#         (ssh icnl_lrd_vm$i "cd /home/docker/alpine/nln && git pull")&
-#     fi
-# done
-
-
+export DEBIAN_FRONTEND=noninteractive
 for i in {2..20}; do
-    echo $((i % 5))
-    if test $((i % 3)) -eq 0; then
-        (ssh icnl_lrd_vm$i "cd /home/docker/alpine && git pull && cd setup/ndn && ./ndn_setup.sh")
+    if test $((i % 10)) -eq 0; then
+        (ssh icnl_lrd_vm$i 'cd /home/docker/alpine/nln && git pull')
     else
-        (ssh icnl_lrd_vm$i "cd /home/docker/alpine && git pull && cd setup/ndn && ./ndn_setup.sh")&
+        (ssh icnl_lrd_vm$i 'cd /home/docker/alpine/nln && git pull')&
     fi
 done
 
 
-# (ssh icnl_lrd_vm2 "cd /home/docker/alpine/nln && git pull && ./package.sh")&
-# (ssh icnl_lrd_vm3 "cd /home/docker/alpine/nln && git pull && ./package.sh")&
-# (sshpass -p password ssh -l docker 172.20.0.6 cd alpine/nln && git pull &)&
-# (sshpass -p password ssh -l docker 172.20.0.7 cd alpine/nln && git pull &)&
-# (sshpass -p password ssh -l docker 172.20.0.8 cd alpine/nln && git pull &)&
-# (sshpass -p password ssh -l docker 172.20.0.9 cd alpine/nln && git pull &)&
-# (sshpass -p password ssh -l docker 172.20.0.10 cd alpine/nln && git pull &)&
-# (sshpass -p password ssh -l docker 172.20.0.11 cd alpine/nln && git pull &)&
-# (sshpass -p password ssh -l docker 172.20.0.12 cd alpine/nln && git pull &)&
-# (sshpass -p password ssh -l docker 172.20.0.13 cd alpine/nln && git pull &)&
-# (sshpass -p password ssh -l docker 172.20.0.14 cd alpine/nln && git pull &)&
-# (sshpass -p password ssh -l docker 172.20.0.15 cd alpine/nln && git pull &)&
-# (sshpass -p password ssh -l docker 172.20.0.16 cd alpine/nln && git pull &)&
-# (sshpass -p password ssh -l docker 172.20.0.17 cd alpine/nln && git pull &)&
-# (sshpass -p password ssh -l docker 172.20.0.18 cd alpine/nln && git pull &)&
-# (sshpass -p password ssh -l docker 172.20.0.19 cd alpine/nln && git pull &)&
-# (sshpass -p password ssh -l docker 172.20.0.20 cd alpine/nln && git pull &)&
+# for i in {2..20}; do
+#     if test $((i % 10)) -eq 0; then
+#         (ssh icnl_lrd_vm$i 'cd /home/docker/alpine && git pull && cd setup/ndn && bash ndn_setup.sh')
+#     else
+#         (ssh icnl_lrd_vm$i 'cd /home/docker/alpine && git pull && cd setup/ndn && bash ndn_setup.sh')&
+#     fi
+# done
+
+# for i in {2..20}; do
+#     if test $((i % 10)) -eq 0; then
+#         (ssh icnl_lrd_vm$i 'cd /home/docker/alpine && git pull && cd nln && bash package.sh')
+#     else
+#         (ssh icnl_lrd_vm$i 'cd /home/docker/alpine && git pull && cd nln && bash package.sh')&
+#     fi
+# done
+
+
+# (ssh icnl_lrd_vm2 "cd /home/docker/alpine/nln && bash run.sh")&
+# (ssh icnl_lrd_vm3 "cd /home/docker/alpine/nln && bash run.sh")&
+# (ssh icnl_lrd_vm4 "cd /home/docker/alpine/nln && bash run.sh")&
+# (ssh icnl_lrd_vm5 "cd /home/docker/alpine/nln && bash run.sh")&
+# (ssh icnl_lrd_vm6 "cd /home/docker/alpine/nln && bash run.sh")&
+# (ssh icnl_lrd_vm7 "cd /home/docker/alpine/nln && bash run.sh")&
+# (ssh icnl_lrd_vm8 "cd /home/docker/alpine/nln && bash run.sh")&
+# (ssh icnl_lrd_vm9 "cd /home/docker/alpine/nln && bash run.sh")&
+# (ssh icnl_lrd_vm10 "cd /home/docker/alpine/nln && bash run.sh")&
+# (ssh icnl_lrd_vm11 "cd /home/docker/alpine/nln && bash run.sh")&
+# (ssh icnl_lrd_vm12 "cd /home/docker/alpine/nln && bash run.sh")&
+# (ssh icnl_lrd_vm13 "cd /home/docker/alpine/nln && bash run.sh")&
+# (ssh icnl_lrd_vm14 "cd /home/docker/alpine/nln && bash run.sh")&
+# (ssh icnl_lrd_vm15 "cd /home/docker/alpine/nln && bash run.sh")&
+# (ssh icnl_lrd_vm16 "cd /home/docker/alpine/nln && bash run.sh")&
+# (ssh icnl_lrd_vm17 "cd /home/docker/alpine/nln && bash run.sh")&
+# (ssh icnl_lrd_vm18 "cd /home/docker/alpine/nln && bash run.sh")&
+# (ssh icnl_lrd_vm19 "cd /home/docker/alpine/nln && bash run.sh")&
+# (ssh icnl_lrd_vm20 "cd /home/docker/alpine/nln && bash run.sh")
 
