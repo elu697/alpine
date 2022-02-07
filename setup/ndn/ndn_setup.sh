4123f63dfdb256650f1efdd2fe0968216807cea8
elu697 <elyou.dev@gmail.com>
2022-02-08 02:00:07 +0900

    Update

--- setup/ndn/ndn_setup.sh
+++ setup/ndn/ndn_setup.sh
@@ -22,7 +22,7 @@ git clone --recursive https://github.com/named-data/jndn.git
 
 sudo dpkg --configure -a
 sudo aptitude update -y
-sudo aptitude upgrade -y
+sudo -E aptitude upgrade -y
 sudo aptitude install -y make libjsoncpp-dev libjsonrpccpp-dev libgmp-dev libreadline-dev libmicrohttpd-dev ocl-icd-libopencl1 \
   opencl-headers mesa-common-dev build-essential libboost-all-dev libsm6 libxext6 libxrender-dev libssl-dev \
   zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev xz-utils tk-dev \
