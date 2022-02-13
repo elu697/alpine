f499cb5c77532c324c05b91398cdc46cfd24007e
elu697 <elyou.dev@gmail.com>
2022-02-14 03:54:12 +0900

    Update

--- nln/run.sh
+++ nln/run.sh
@@ -2,8 +2,7 @@ set -Ceuxo pipefail
 export DEBIAN_FRONTEND=noninteractive
 PASSWORD=password
 echo $PASSWORD | sudo -S ls
-sudo nfd-stop
-sudo nfd &
+sudo nfd-start &
 
 cd core
 java -jar target/core-1.0.jar $@
