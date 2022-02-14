45b66c8ba8ac5e76eb790d6cadfa64e5a4d92616
elu697 <elyou.dev@gmail.com>
2022-02-14 13:44:22 +0900

    Update

--- nln/run.sh
+++ nln/run.sh
@@ -2,9 +2,11 @@ set -Ceuxo pipefail
 export DEBIAN_FRONTEND=noninteractive
 PASSWORD=password
 echo $PASSWORD | sudo -S ls
+
 sudo nfd-start &
 
 cd core
+sudo chown docker:docker target/core-1.0.jar
 java -jar target/core-1.0.jar $@
 cd ..
 exit
