2fb71b4f3095c9d747d6e91575a3d0214f2ab910
elu697 <elyou.dev@gmail.com>
2022-02-15 22:24:06 +0900

    Update

--- nln/stop.sh
+++ nln/stop.sh
@@ -4,11 +4,5 @@ PASSWORD=password
 echo $PASSWORD | sudo -S ls
 # sudo aptitude install -y maven
 # sudo systemctl restart docker
-# sudo nfd-stop &
-# sleep 1
-sudo nfd-start &
-sleep 3
-cd core
-java -jar target/core-1.0.jar $@
-cd ..
+sudo nfd-stop &
 exit
