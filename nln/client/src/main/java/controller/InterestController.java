ef46cc3dd86c4811314d42bf5fc5559c4833daad
elu697 <elyou.dev@gmail.com>
2022-02-20 09:04:55 +0000

    Update

---
+++ nln/client/src/main/java/controller/InterestController.java
@@ -0,0 +1,46 @@
+package controller;
+
+import model.ResponseData;
+import ndn.Controller;
+import net.named_data.jndn.*;
+
+import java.util.function.Consumer;
+
+public class InterestController {
+    Controller ndnController;
+
+    public InterestController() {
+        ndnController = new Controller();
+    }
+
+    public static void main(String[] args) {
+        InterestController interestController = new InterestController();
+        interestController.request("/model/A", responseData -> {
+            System.out.println(responseData.getPojo());
+        });
+    }
+
+    public void request(String uri, Consumer<ResponseData> responseDataConsumer) {
+        ndnController.interest(uri, true, true, new OnData() {
+            @Override
+            public void onData(Interest interest, Data data) {
+                ResponseData responseData = new ResponseData(data.getContent().toString());
+                responseDataConsumer.accept(responseData);
+                ndnController.endLoop();
+            }
+        }, new OnTimeout() {
+            @Override
+            public void onTimeout(Interest interest) {
+                responseDataConsumer.accept(ResponseData.returnEmpty());
+                ndnController.endLoop();
+            }
+        }, new OnNetworkNack() {
+            @Override
+            public void onNetworkNack(Interest interest, NetworkNack networkNack) {
+                responseDataConsumer.accept(ResponseData.returnEmpty());
+                ndnController.endLoop();
+            }
+        });
+        Controller.setDaemonThread(ndnController::runLoop);
+    }
+}
